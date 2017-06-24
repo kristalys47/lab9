0ad96ab7723c32aab0a528e46e3e25b3aa9c74d8
kristalys47 <kristalys.cba4@yahoo.com>
2017-06-24 14:24:48 -0400

    jijiji

--- lab9/src/RecursiveSet.java
+++ lab9/src/RecursiveSet.java
@@ -1,6 +1,36 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RecursiveSet {
-	
+
+	public static void main(){
+		Scanner input = new Scanner(System.in);
+		System.out.print("ENter a short string of different letters:");
+		String s = input.nextLine();
+		ArrayList<String> ss = subsets(s);
+		System.out.println("Subset of set formed by letter in" + s + "are:");
+		for (String r: ss){
+			System.out.println(" \"" + r +"\"");
+		}
+	}
+		private static ArrayList<String> subsets(String s){
+			ArrayList<String> ss = new ArrayList<String>();
+			if (s.length() == 0){
+				ss.add(s);
+			}
+			else {
+				String st;
+				st = s.substring(1);
+				ArrayList<String> tSS = subsets(st);
+				for (String r : tSS)
+					ss.add(__________);
+				for (String r : tSS)
+					ss.addAll(__________ +r);
+			}
+			return ss;
+			
+		}
+		
+		
 
 }
